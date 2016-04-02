7cc02f00e21d65006f94e068554839c814b2f530
Michael Duarte <michael.duarte@hotmail.com>
2016-04-02 03:31:10 -0700

    refactored redblueblueredDropbox.cpp
    - use a map to associate each char of the pattern with a data structure which keeps track of possible matches, instead of having 3 different maps and vectors to keep track of the same data.
    
    -removed repeated code and commented out the debugging output.

--- redblueblueredDropbox.cpp
+++ redblueblueredDropbox.cpp
@@ -29,32 +29,46 @@
  NOTE: The input does not need to contain actual "dictionary words". "abcxyzxyzabc" would also be an input that matches the pattern "abba".
  */
 
-#include <queue>
 #include <iostream>
 #include <unordered_map>
+#include <stack>
 using namespace std;
 
-/*
- * Complete the function below.
- */
+struct PatternInfo{
+    char character;
+    int repetitions;
+    stack<string> tentativeMatches;
+    
+    void clearProgress(){
+        while(tentativeMatches.size())
+            tentativeMatches.pop();
+    }
+    
+    bool trySmallerSubstring(){
+        if (tentativeMatches.size() < 2 )
+            return false;
+        
+        // we are gonna re-evaluate this pattern character
+        tentativeMatches.pop();  // with a smaller substring
+        ++repetitions;  // we have to fin another repetition
+        return true;
+    }
+    
+    inline bool hasTentativeMatches() const{ return tentativeMatches.size();}
+    
+    /* Check if there are tentative matches before trying to get one */
+    inline const string &getTentativeMatch() const{ return tentativeMatches.top(); }
+};
 
-
-unordered_map<char, queue<unsigned> > patternReps;
-unordered_map<char, string > patternMap;
-vector<int> lengthLimiter;
+unordered_map<char, PatternInfo> m_data; // data owner
 
 void initMaps(const string &pattern, const string&input){
-    patternReps.clear();
-    patternMap.clear();
+    m_data.clear();
     for(unsigned i = 0; i < pattern.length(); ++i){
-        auto &queue = patternReps[pattern[i]];
-        queue.push(i);
+        auto &info = m_data[pattern[i]];
+        info.character = pattern[i];
+        ++info.repetitions;
     }
-}
-
-void clearLengthLimiter(const string &pattern){
-    // fill vector with as many 0s as letters has the pattern.
-    lengthLimiter = vector<int>(pattern.length(), 0);
 }
 
 int getSubstrRepsInInput(const string &input, const string& substr, size_t startingPos){
@@ -72,34 +86,27 @@ int getSubstrRepsInInput(const string &input, const string& substr, size_t start
             // cout << startingPos << ", ";
         }
     } while ( (startingPos+substrLen <= input.length()) );
-    cout << endl;
+//    cout << endl;
     return reps;
 }
 
-void decreasePatternRep(unordered_map<char, queue<unsigned> > &reps, char c){
-    auto &repQueue = reps[c];
-    repQueue.pop();
-    if (repQueue.size() == 0){
-        reps.erase(c);
-    }
-}
-
-void incrementPatternRep(unordered_map<char, queue<unsigned> > &reps, char c, int val = -1){
-    auto &repQueue = reps[c];
-    repQueue.push(val);
-}
-
 bool tryCorrectingLastMatch(size_t &patternIndex, size_t &startPoint, const string &pattern){
-    if (patternIndex == 0){
+    char previousChar = pattern[patternIndex - 1];
+    auto &info = m_data[previousChar];
+    if (patternIndex == 0 || !info.hasTentativeMatches()){
         // if we cannot find a match for the first letter, we are done.
         return false;
     }
-    for (size_t i = patternIndex ; i < lengthLimiter.size() ;++i ) lengthLimiter[i] = 0; // restart the limiters after this index.
-    ++lengthLimiter[--patternIndex];
-    incrementPatternRep(patternReps, pattern[patternIndex]); // add another repetition to previous pattern character.
-    startPoint -= patternMap[pattern[patternIndex]].length(); // subtract the match's length to the starting point.
-    patternMap.erase(pattern[patternIndex]); // delete match from map.
-    --patternIndex; // re-evaluate previous character, we go back 2 index bc we increment at the end of the loop
+    for (size_t i = patternIndex ; i < pattern.length() ;++i )
+        if (pattern[i] != previousChar)
+            m_data[pattern[i]].clearProgress(); // restart the progress from current index on.
+    
+    
+    size_t lastMatchLen = info.getTentativeMatch().length();
+    if(!info.trySmallerSubstring())
+        return false;
+    startPoint -= lastMatchLen;  // subtract the match's length to the starting point.
+    patternIndex -= 2;  // re-evaluate previous character, we go back 2 index bc we increment at the end of the loop
     return true;
 }
 
@@ -107,16 +114,15 @@ bool followsPattern(string &pattern, string &input){
     size_t accumLength = 0;
     //iterate thru each pattern letter to find its corresponding match
     for(size_t i = 0; i < pattern.length(); ++i){
-        // vector keeping track of all the substrings matching a letter from the pattern
-        vector<string> matches;
-        
+        char currentChar = pattern[i];
+        auto &info = m_data[currentChar];
         //if there is a match for the letter being checked, prove that it matches
-        const string &section = patternMap[pattern[i]];
-        if (!section.empty()){
-            cout << "checking if " << pattern[i] << " == " << section << endl;
+        if (info.hasTentativeMatches()){
+            const string &match = info.getTentativeMatch();
+//            cout <<"checking if "<< currentChar <<" == "<< match;
             bool didMatch = true;
-            for (int t = 0 ; t < section.length() && accumLength+t < input.length(); ++t){
-                if (section[t] != input[accumLength+t]){
+            for (int t = 0 ; t < match.length() && accumLength+t < input.length(); ++t){
+                if (match[t] != input[accumLength+t]){
                     //there is a descripancy, explode... or try fixing it.
                     didMatch = false;
                     if (tryCorrectingLastMatch(i, accumLength, pattern))
@@ -125,18 +131,19 @@ bool followsPattern(string &pattern, string &input){
                         return false;
                 }
             }
-            // they match! check next pattern's character.
+            
+//            cout << (didMatch?" YESS":" no :(")<<endl;
+            
             if(didMatch){
-                decreasePatternRep(patternReps, pattern[i]);
-                accumLength += section.length();
+                // they match! check next pattern's character.
+                --info.repetitions;
+                accumLength += match.length();
             }
             continue;
         }
-        //Try to find a match for this pattern's character
-        auto &repQueue = patternReps[pattern[i]];
         
-        //try different string lengths to substring the input and count the amount
-        //of times it is repeated in the input.
+        // Find a match for this pattern's character, try different string lengths to substring the input and
+        // count the amount of times it is repeated in the input.
         size_t maxLen = input.length() - accumLength + 1;
         bool foundMatch = false;
         for (size_t len = 1; len < maxLen; ++len){
@@ -145,36 +152,25 @@ bool followsPattern(string &pattern, string &input){
             //count number of times the substring is repeated in the input.
             int reps = getSubstrRepsInInput(input, substr, newPos);
             
-            if (reps == repQueue.size()){
+            if (reps == info.repetitions){
                 //if the # of reps is the same as the pattern's character amount of reps
                 //we might have found a match!, but let's keep trying to make the substring larger
-                matches.push_back(substr);
-                cout <<"\t'"<<pattern[i] << "' possibly: "  << substr << endl;
+                info.tentativeMatches.push(substr);
+//                cout <<"\t'"<< currentChar <<"' possibly: "<< substr <<endl;
                 
+                // do not consume all the characters, leave at least as many as remaining characters in the pattern
                 if ((maxLen-len-1 >= pattern.length() - i))
                     continue;
             }
             
-            if ( reps <= repQueue.size()){
-                // if get a substring with less reps than the pattern's character, making the substring larger won't help, break.
-                if (!matches.empty()){
-                    //once we start getting not enough repetitions but we once did get enough,
-                    //we take the last good substring as the match!
-                    size_t matchIndex = matches.size() - 1 - lengthLimiter[i];
-                    if (matchIndex >= matches.size()){
-                        cout << "potential substrings: " << matches.size() << " , skipping last " << lengthLimiter[i] <<" options"<<endl;
-                        break;  //we ran out of possibilities for this character.
-                    }
-                    
-                    const string & match = matches[matchIndex];
-                    //save the match for future reference.
-                    accumLength += match.length();
-                    patternMap[pattern[i]] = match;
-                    
-                    //decrement the number of times this pattern's char repeated
-                    //since we just finished matching one.
-                    decreasePatternRep(patternReps, pattern[i]);
-                    cout << pattern[i] << " == "  << match <<". Next starting pos: " << accumLength<< endl << endl;
+            if ( reps <= info.repetitions){
+                //once we start getting not enough repetitions but we once did get enough,
+                //we take the last good substring as the match!
+                if (info.hasTentativeMatches()){
+                    //decrement the number of times this pattern's char repeated since we just finished matching one.
+                    --info.repetitions;
+                    accumLength += info.getTentativeMatch().length();
+//                    cout << currentChar <<" == "<< info.getTentativeMatch() <<". Next starting pos: "<< accumLength<<endl<<endl;
                     foundMatch = true;
                 }
                 break;
@@ -184,32 +180,31 @@ bool followsPattern(string &pattern, string &input){
         if(!foundMatch){
             //Not finding a match could mean that this input is not following the pattern
             //or that one of the precious matches grabbed more characters than it should have.
-            // i.e. pattern: abab, input: redblueredblue
-            // result: a = redbl, b= ue. it works, but not as a human would've solve it.
-            cout << "Match not found for letter " << pattern[i] << " trying to re-evaluate previous pattern" << endl;
+            // i.e. pattern: abab, input: redblueredblue -->> result: a = redbl, b= ue. it works, but not as a human would've solve it.
+//            cout << "Match not found for letter " << pattern[i] << " trying to re-evaluate previous pattern" << endl;
             if (!tryCorrectingLastMatch(i, accumLength, pattern))
                 return false;
         }
     }
-    return patternReps.empty()?true:false;
+    return true;
 }
 
 bool wordpattern(string &pattern, string &input) {
     initMaps(pattern,input);
-    clearLengthLimiter(pattern);
     return followsPattern(pattern,input);
 }
 
 int main() {
     // abba - redblueredblue = NO
+    // abba - redbluebluered = OK
     // abab - redblueredblue = OK
     // abb  - redblueblue = OK
     cout << "Pattern: ";
-    string _pattern = "abba";
+    string _pattern = "abab";
     // cin >> _pattern;
     
     cout << "Input: ";
-    string _input = "redbluebluered";
+    string _input = "redblueredblue";
     // cin >> _input;
     
     cout << "pattern: " << _pattern << ", input: " << _input << endl;
