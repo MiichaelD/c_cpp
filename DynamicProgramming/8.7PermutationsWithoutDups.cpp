185b396d02c61fd3dbf4985326eafabd6ebb5bac
Michael Duarte <michael.duarte@hotmail.com>
2019-09-22 12:20:31 -0700

    Left comments describing what each approach is doing

--- DynamicProgramming/8.7PermutationsWithoutDups.cpp
+++ DynamicProgramming/8.7PermutationsWithoutDups.cpp
@@ -13,8 +13,17 @@
 #include <memory>
 
 using namespace std;
+
+/**
+ *  Prints a list of permutations of the input string.
+ *  This solution uses more recursion.
+ *  Recursively calls itself by removing a letter of the input string in order, this remainder is
+ *  used as the new input string to permutate, while the removed character is appended at the end of
+ *  the prefix.
+ *  Once the input string to permutate is empty, we print the prefix.
+ */
 void printPermutations(const string &str, const string &prefix = ""){
-	if (str.length() == 0) {
+	if (str.empty()) {
 		cout << prefix << endl;
 	}
 	else {
@@ -26,19 +35,27 @@ void printPermutations(const string &str, const string &prefix = ""){
 	}
 }
 
+/**
+ *  Returns a list of permutations of the input string.
+ *  This solution uses less recursion, it goes as deep as many characters are in the string.
+ *  For each word returned by recursively calling the same function with the remainder of input
+ *  string (after removing the 1st character), the 1st character will be moved around each position
+ *  of the word.
+ *  Once the 1st character has been permutated in each position for each word returned by the
+ *  previous recursive call, we return the new resulting array to be used as new list of words.
+ */
 deque<string> getPermutations(const string &str){
 	deque<string> result;
 	if (str.empty()){
 		result.push_back(str);
 		return result;
 	}
-
 	char first = str[0];
-	deque<string> words = getPermutations(str.substr(1));
-	for (string &word : words){
+	deque<string> seedWords = getPermutations(str.substr(1));
+	for (string &word : seedWords){
 		for (int i = 0; i <= word.length(); ++i){
-			string newWord = word.substr(0, i) + first + word.substr(i);
-			result.push_back(newWord);
+			result.push_back(word.substr(0, i) + first + word.substr(i));
+      // cout << "pushing back: " << result.back() << endl;
 		}
 	}
 	return result;
@@ -52,7 +69,7 @@ void print(const deque<string> &deck){
 }
 
 int main(){
-	string str = "abc";
+	string str = "123";
 	printPermutations(str);
 	cout << endl << endl;
 	auto permutations = getPermutations(str);
